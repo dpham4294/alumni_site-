38a15fe574195423b71f80d79d87047f5c329fc4
Diamond Pham <dpham4294@gmail.com>
2017-09-18 20:38:15 +0000

    sizing updated

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-if(screen.width>=1000) {
+if(screen.width<=1000) {
     document.write("<style>body{zoom:90%;}</style>");
 }
 if(screen.width>=1300) {
     document.write("<style>body{zoom:100%;}</style>");
 }
-if(screen.width>=1500) {
+if(screen.width>=1700) {
     document.write("<style>body{zoom:115%;}</style>");
 }
 if(screen.width>=2000) {
